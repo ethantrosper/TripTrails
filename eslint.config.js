58fb1ac7a90dd42ee59169b73843c095454404d8
Ethan Trosper <ethancoletrosper@gmail.com>
2024-09-25 13:09:03 -0400

    Expo ES lint setup. Renaming .eslintrc.js to .depreciatedeslintrc.js because this was the old declaritive style of ESLint.

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,30 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  js.configs.recommended,
+
+ {
+     rules: {
+         "no-unused-vars": "warn",
+         "no-undef": "warn"
+     }
+ }
+];
+
+/*
+export default [
+
+  in files I had to escape out the * because the parser was
+  thinking that it was the end of the comment which it was not
+
+  { files: ["*//*/*.{js,mjs,cjs,ts,tsx}"] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
 ];
+*/
